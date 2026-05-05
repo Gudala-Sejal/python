8c9053272d13eb126120e080029bb95761e328e1
Gudala-Sejal <2400033274@kluniversity.in>
2026-05-05 19:44:59 +0530

    Add files via upload

---
+++ Sample.py
@@ -0,0 +1,13 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print(10+10)
+20
+>>> print("python")
+python
+>>> Print("Hello")
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    Print("Hello")
+NameError: name 'Print' is not defined. Did you mean: 'print'?
+>>> print("Sample")
+Sample
