4dbdd8f00173dc3f05c7867a8a69b2d14144be72
Gudala-Sejal <2400033274@kluniversity.in>
2026-05-06 19:18:10 +0530

    Add files via upload

---
+++ Datatypes1.py
@@ -0,0 +1,38 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=5
+>>> type(a)
+<class 'int'>
+>>> b=8.9
+>>> type(b)
+<class 'float'>
+>>> c='code'
+>>> type(c)
+<class 'str'>
+>>> d="codegyan"
+>>> type(d)
+<class 'str'>
+>>> print(type(d)) #In interpreter if we don't mention printit gives output but not in compiler.We have mention print
+<class 'str'>
+>>> e='''python'''
+>>> type(e)
+<class 'str'>
+>>> d="p"
+>>> type(d)
+<class 'str'>
+>>> x=7+9j
+>>> type(x)
+<class 'complex'>
+>>> a=7+9i
+SyntaxError: invalid decimal literal
+>>> y=True
+>>> type(y)
+<class 'bool'>
+>>> x=False
+>>> type(x)
+<class 'bool'>
+>>> x="true"
+>>> print(x)
+true
+>>> type(x)
+<class 'str'>
