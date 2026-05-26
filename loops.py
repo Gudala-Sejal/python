657c43afb8d9c51bd3519a79f69b086c0ad86bf7
Gudala-Sejal <2400033274@kluniversity.in>
2026-05-26 21:29:28 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,107 @@
+#for loop
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i,end=" ")'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(a)'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+#tuple
+'''a=(10,20,30,40,50)
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+#set
+'''a={10,20,30,40,50}
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+#dictionary
+'''a={"year":2026,"month":"may","date":16}
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+'''a={"year":2026,"month":"may","date":16}
+
+
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))
+a={"year":2026,"month":"may","date":16}
+for i in a.keys():
+    print(i)
+    print(type(a))
+    print(type(i))
+    a={"year":2026,"month":"may","date":16}
+for i in a.values():
+    print(i)
+    print(type(a))
+    print(type(i))
+    a={"year":2026,"month":"may","date":16}
+for i in a.items():
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+#string
+'''a="codegnan"
+for i in a:
+    print(i)'''
+
+'''a="codegnan"
+for i in a:
+   print(i,end=" ")'''
+
+'''a=[4.5,6.7]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+'''a=[4+9j,5+16j,22+56j]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+'''a=[True,False]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+#Task
+'''a=["code","codegnan","python"]
+b=str(a)
+c=b.upper()
+for i in c:
+    print(i,end="")'''
+
+
+
+
+
