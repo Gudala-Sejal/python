cd6577176317f9d4713d651ccfc6b101af929be6
Gudala-Sejal <2400033274@kluniversity.in>
2026-05-27 20:01:10 +0530

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,66 @@
+'''a=10
+b=20
+print("The sum is: ",a+b)
+print("The difference is: ",a-b)
+print("The product is: ",a*b)
+a=100
+b=200
+print("The sum is: ",a+b)
+print("The difference is: ",a-b)
+print("The product is: ",a*b)
+a=1000
+b=2000
+print("The sum is: ",a+b)
+print("The difference is: ",a-b)
+print("The product is: ",a*b)'''
+
+
+#in above code the lines of code was incresing to reduce that we use FUNCTIONS
+#FUNCTIONS
+'''def calculate(a,b):
+    print("The sum is: ",a+b)
+    print("The difference is: ",a-b)
+    print("The product is: ",a*b)
+calculate(10,20)
+calculate(100,200)
+calculate(1000,2000)'''
+
+
+'''def calculate(a,b):
+    print("The integer division value is: ",a//b)
+    print("The modulo division is: ",a%b)
+    print("The power is: ",a**b)
+calculate(2,3)
+calculate(3,4)
+calculate(4,5)'''
+
+'''def add(a,b):
+    print(a+b)
+add(4,5)'''
+
+'''def add():
+    a=int(input("enter a value"))
+    b=int(input("enter b value"))
+    print(a+b)
+add()'''
+
+'''def fullname():
+    a=input("First name:")
+    b=input("Last name:")
+    print((a+" " +b).title())
+fullname()'''
+
+
+'''while True:
+    def calculate():
+        a=int(input("Enter a"))
+        b=int(input("Enter b"))
+        options=int(input("Choose option 1.add 2.difference 3.product"))
+        if options==1:
+            print(a+b)
+        if options==2:
+            print(a-b)
+        if options==3:
+            print(a*b)
+    calculate()'''      
+
