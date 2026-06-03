aa595105040caa859582b38528b7ea659fc9fb27
Gudala-Sejal <2400033274@kluniversity.in>
2026-06-03 21:39:50 +0530

    Add files via upload

---
+++ mymodule.py
@@ -0,0 +1,52 @@
+'''def greetings(name):
+    print("welcome",name)'''
+
+'''a=4
+b=7
+c=a+b
+print("the sum is: ",c)'''
+
+'''d={"idnos":[10,20,30],
+   "names":["sejal","deepthi","haasini"],
+   "marks":[60,70,80]}'''
+
+'''if __name__=="__main__":
+    a=[10,20,30,40,50]
+    #a.append("code")
+    a.extend("code")
+    print(a)'''
+
+'''def dummy():
+    if __name__=="__main__":
+        print("this program is run as script")
+    else:
+        print("this program is run as module")
+dummy()'''
+
+'''import math
+print(math.pi)
+print(math.pi*5)
+print(math.sqrt(2))
+print(math.log(2))
+print(math.tan(45))
+print(math.sin(30))
+print(math.cos(60))
+print(math.pow(2,4))
+print(math.ceil(5.8))
+print(math.floor(2.9))'''
+
+
+#sys module
+'''import sys
+print(sys.path)
+for i in sys.path:
+    print(i)'''
+
+
+#os module
+'''import os
+print(os.path)
+print(os.getcwd())   #cwd- command working directory
+print(os.listdir())
+print(os.mkdir("june3"))'''
+
---
+++ mymodules import.py
@@ -0,0 +1,13 @@
+'''import mymodule
+mymodule.greetings("sejal")'''
+
+'''import mymodule'''
+
+'''import mymodule
+b=mymodule.d["idnos"]
+c=mymodule.d["names"]
+print(b)
+print(c)'''
+
+
+#import mymodule
